f163e30a6f31ff7a92be6030a44d59ee5d6cc002
vaguiiinho <avanlendario@gmail.com>
2024-11-13 20:42:12 -0300

    Banner Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className='relative h-full lg:h-[140vh]'>
       <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
-          <div className=' absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'></div>
+          <div className=' absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
+            <Image
+              src='/img1.jpg'
+              alt='The Witcher'
+              fill={true}
+              className="object-cover object-top h-[65vh] lg:h-[95vh]"
+            />
+          </div>
           <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>
             The Witcher
           </h1>
@@ -13,12 +21,14 @@ export default function Home() {
             place in a world where people often prove more wicked than beasts.
           </p>
         </div>
-        <button className='w-24 rounded-full bg-white px-4 py-2 font-bold text-black md:w-32 lg:w-40'>
-          Play
-        </button>
-        {/* <button className='w-24 rounded-full bg-gray-700 px-4 py-2 font-bold text-white md:w-32 lg:w-40'>
-          More info
-        </button> */}
+        <div className="flex space-x-3 ">
+          <button className="md:text-xl flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5">
+            Play
+          </button>
+          <button className="md:text-xl flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5">
+            More Info
+          </button>
+        </div>
       </main>
     </div>
   );
