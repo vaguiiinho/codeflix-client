7dacbc36480329fcf03b8ef287b266813d30f941
vaguiiinho <avanlendario@gmail.com>
2024-11-15 14:48:38 -0300

    Header Modular 1

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react'
 
 export default function Header() {
@@ -19,7 +20,13 @@ export default function Header() {
     return (
         <header className={`${isScrolled && 'bg-black'} fixed top-0 z-50  flex items-center justify-between  w-full transition-all px-4 py-4 lg:px-10 lg:py-6`}>
             <div className='flex items-center space-x-2 md:space-x-8'>
-                <img src="https://rb.gy/ulxxee" alt="netflix" width={120} height={120} />
+                <Image
+                    src='/logo.svg'
+                    alt='Logo'
+                    width={90}
+                    height={90}
+                    className='cursor-pointer'
+                />
                 <ul className='hidden md:flex md:space-x-4'>
                     <li>Home</li>
                     <li>TV Shows</li>
@@ -30,7 +37,13 @@ export default function Header() {
             </div>
             <div className='flex items-center space-x-4'>
                 <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
-                <img src="https://rb.gy/g1pwyx" alt="" className='cursor-pointer rounded' />
+                <Image
+                    src="/profile.png"
+                    alt=""
+                    width={40}
+                    height={40}
+                    className='cursor-pointer rounded'
+                />
             </div>
         </header>
     )
