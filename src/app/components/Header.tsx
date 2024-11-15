02e9aec17f4ddd8c254714752d6d05988c42000b
vaguiiinho <avanlendario@gmail.com>
2024-11-15 13:58:03 -0300

    Header Profile

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <header className='fixed top-0 z-50  flex items-center justify-between w-full bg-black px-4 py-4 lg:px-10 lg:py-6'>
+        <header className='fixed top-0 z-50  flex items-center justify-between  w-full transition-all px-4 py-4 lg:px-10 lg:py-6'>
             <div className='flex items-center space-x-2 md:space-x-8'>
                 <img src="https://rb.gy/ulxxee" alt="netflix" width={120} height={120} />
                 <ul className='hidden md:flex md:space-x-4'>
@@ -12,6 +12,10 @@ export default function Header() {
                     <li>Latest</li>
 
                 </ul>
+            </div>
+            <div className='flex items-center space-x-4'>
+               <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
+               <img src="https://rb.gy/g1pwyx" alt="" className='cursor-pointer rounded' />
             </div>
         </header>
     )
