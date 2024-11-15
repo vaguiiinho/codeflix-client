ad727100cf74cff2220ee9a0358d14ee8279d0e3
vaguiiinho <avanlendario@gmail.com>
2024-11-15 14:14:31 -0300

    Header Hook

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 
 export default function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        }
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
-        <header className='fixed top-0 z-50  flex items-center justify-between  w-full transition-all px-4 py-4 lg:px-10 lg:py-6'>
+        <header className={`${isScrolled && 'bg-black'} fixed top-0 z-50  flex items-center justify-between  w-full transition-all px-4 py-4 lg:px-10 lg:py-6`}>
             <div className='flex items-center space-x-2 md:space-x-8'>
                 <img src="https://rb.gy/ulxxee" alt="netflix" width={120} height={120} />
                 <ul className='hidden md:flex md:space-x-4'>
@@ -14,8 +29,8 @@ export default function Header() {
                 </ul>
             </div>
             <div className='flex items-center space-x-4'>
-               <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
-               <img src="https://rb.gy/g1pwyx" alt="" className='cursor-pointer rounded' />
+                <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
+                <img src="https://rb.gy/g1pwyx" alt="" className='cursor-pointer rounded' />
             </div>
         </header>
     )
