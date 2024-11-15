516a3034bf50f3ce43e2ff38def05eeb07509a21
vaguiiinho <avanlendario@gmail.com>
2024-11-15 17:23:19 -0300

    Icones E Plugins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Header from "./components/Header";
+import { PlayIcon } from "@heroicons/react/24/solid";
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
   return (
@@ -25,9 +27,11 @@ export default function Home() {
         </div>
         <div className="flex space-x-3 ">
           <button className="md:text-xl flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5">
+            <PlayIcon className="h-6" />
             Play
           </button>
           <button className="md:text-xl flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5">
+            <InformationCircleIcon className="h-6" />
             More Info
           </button>
         </div>
