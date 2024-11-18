eb76bb83b3e2bea3f02c9e3101b7610207eeaafd
vaguiiinho <avanlendario@gmail.com>
2024-11-18 20:56:19 -0300

    Page Login 1

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div
+      style={{ backgroundImage: 'url(/background.jpg)' }}
+      className='bg-cover bg-center bg-no-repeat text-white '>
+      <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+        Pagina de Login
+      </div>
+    </div>
+  )
+}
